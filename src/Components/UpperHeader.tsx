5e2e925e5d46268dd2d750a21f45ce5204f2cf0d
faheem221 <faheemk793@gmail.com>
2025-02-11 17:43:29 +0530

    fixed overlay

--- src/Components/UpperHeader.tsx
+++ src/Components/UpperHeader.tsx
@@ -1,6 +1,6 @@
 const UpperHeader = () => {
   return (
-    <div className='w-screen top-0 left-0 z-20 fixed py-1 flex justify-center bg-pink-600 '>
+    <div className='w-screen top-0 left-0 z-[999] fixed py-1 flex justify-center bg-pink-600 '>
         <img className='w-10' src={'/media/Logo-banner.png'} />
     </div>
   )
