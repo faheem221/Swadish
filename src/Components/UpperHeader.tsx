b9ec497e44c06afb958c1547e0004a83cc4f66f3
faheem221 <faheemk793@gmail.com>
2025-02-12 13:23:26 +0530

    fixed upper header

--- src/Components/UpperHeader.tsx
+++ src/Components/UpperHeader.tsx
@@ -1,9 +1,37 @@
-const UpperHeader = () => {
-  return (
-    <div className='w-screen top-0 left-0 z-[999] fixed py-1 flex justify-center bg-pink-600 '>
-        <img className='w-10' src={'/media/Logo-banner.png'} />
-    </div>
-  )
-}
+import { useEffect, useState } from "react";
 
-export default UpperHeader
+const UpperHeader = () => {
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        // Scrolling Down -> Hide
+        setIsVisible(false);
+      } else {
+        // Scrolling Up -> Show
+        setIsVisible(true);
+      }
+      setLastScrollY(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  return (
+    <div
+      className={`w-screen top-0 left-0 z-[999] fixed py-1 flex justify-center bg-pink-600 transition-transform duration-300 ${
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      }`}
+    >
+      <img className="w-10" src={"/media/Logo-banner.png"} />
+    </div>
+  );
+};
+
+export default UpperHeader;
